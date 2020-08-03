d1e042d57b769e1e0856dfcc9236fad0a2d06147
Wercton <werctonmatheus@gmail.com>
2020-08-03 12:37:10 -0300

    Update itertools_challenges.py

--- Python/itertools_challenges.py
+++ Python/itertools_challenges.py
@@ -59,27 +59,12 @@ def probability_of_a_letter():
 # Maximize It
 def maximizing_it():
 
-numberOfTimes, M = map(int, input().split())
+	numberOfTimes, M = map(int, input().split())
 
-soManyLists = [list(map(lambda x: int(x) ** 2, input().split()[1:])) for _ in range(numberOfTimes)]
-soManyLists = list(map(lambda x: sum(x)%M, list(itertools.product(*soManyLists))))
+	soManyLists = [list(map(lambda x: int(x) ** 2, input().split()[1:])) for _ in range(numberOfTimes)]
+	soManyLists = list(map(lambda x: sum(x)%M, list(itertools.product(*soManyLists))))
 
-print(max(soManyLists))
-
-
-
-'''
-7 867
-7 6429964 4173738 9941618 2744666 5392018 5813128 9452095
-7 6517823 4135421 6418713 9924958 9370532 7940650 2027017
-7 1506500 3460933 1550284 3679489 4538773 5216621 5645660
-7 7443563 5181142 8804416 8726696 5358847 7155276 4433125
-7 2230555 3920370 7851992 1176871 610460 309961 3921536
-7 8518829 8639441 3373630 5036651 5291213 2308694 7477960
-7 7178097 249343 9504976 8684596 6226627 1055259 4880436
-
-866
-'''
+	print(max(soManyLists))
 
 
 if __name__ == '__main__':
